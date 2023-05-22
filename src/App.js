7b4169fa2b526818b62be211eee0c3a70fab1aa9
funnmer <admin@louiml.net>
2023-05-22 18:52:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles.css';
 
-function App() {
+const TouishTranslator = () => {
+  const [englishText, setEnglishText] = useState('');
+  const [touishText, setTouishText] = useState('');
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+
+  const translateToTouish = () => {
+    axios
+      .post('https://touishapi.louiml.net/translate', { text: englishText })
+      .then(response => {
+        console.log('API Response:', response.data);
+        const translatedText = response.data.translation;
+        setTouishText(translatedText);
+      })
+      .catch(error => {
+        console.error('API Error:', error);
+      });
+  };
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(touishText);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${theme}`}>
+      <div className="input-container">
+        <input
+          className={`input-field ${theme}`}
+          type="text"
+          placeholder="Enter text..."
+          value={englishText}
+          onChange={event => setEnglishText(event.target.value)}
+        />
+        <button className={`button ${theme}`} onClick={translateToTouish}>
+          Translate
+        </button>
+      </div>
+      <div className={`result ${theme}`} onClick={copyToClipboard}>
+        {touishText}
+      </div>
+      <button className={`theme-toggle-button ${theme}`} onClick={toggleTheme}>
+        {theme === 'light' ? 'Dark Theme' : 'Light Theme'}
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default TouishTranslator;
